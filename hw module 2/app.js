b2dc4415340273804a060d40ddde139b14821eeb
zikrikn <zikrikn@student.telkomuniversity.ac.id>
2023-07-15 07:24:40 +0700

    tukar posisi route

--- hw module 2/app.js
+++ hw module 2/app.js
@@ -17,8 +17,8 @@ const controller = new playlistController();
 app.post('/playlist', controller.createPlaylist);
 app.post('/playlist/song', controller.addSong);
 app.get('/playlist/:playlistId/songs', controller.getAllSongs);
-app.get('/playlist/:playlistId/songs/sorted', controller.getSortedSongsByPlayCount);
 app.get('/playlist/:playlistId/songs/:songId', controller.getSong);
+app.get('/playlist/:playlistId/songs/sorted', controller.getSortedSongsByPlayCount);
 
 // Start the server
 app.listen(port, () => {
